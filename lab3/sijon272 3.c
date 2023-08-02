243b9c2871ab2de3ee5b6917e1534ff4f073aaf3
Sejonhossain <141204277+Sejonhossain@users.noreply.github.com>
2023-08-02 10:29:01 +0600

    Add files via upload

---
+++ lab3/sijon272 3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    for(;;)
+
+    {
+        printf("%d ",i=i+2);
+        if(i>=72)
+        {
+          break;
+        }
+
+    }
+}
+
+
+
+
+
---
+++ lab3/sijon272.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+        for(k=5,i=1,j=5; i<=10;)
+    {
+
+        printf("%d X %d= %d\n",i,k,j);
+
+        j+=5;
+         i++;
+
+    }
+
+}
